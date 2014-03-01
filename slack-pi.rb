3347b2699b2ed91bf1b206a5347926df8138e582
Dave Goerlich <dgoerlich@designinginteractive.com>
2014-03-01 02:21:14 +0000

    Add a config file for slack

--- slack-pi.rb
+++ slack-pi.rb
@@ -1,25 +1,8 @@
 require_relative 'lib/led'
-
-def flip led
-  led.toggle
-  sleep 0.1
-end
+require_relative 'config/slack_config'
 
 red = Led.new "Red", 18
 green = Led.new "Green", 23
 blue = Led.new "Blue", 24
 yellow = Led.new "Yellow", 25
 
-0.step(11) do
-  flip red
-  flip green
-  flip blue
-  flip yellow
-
-  sleep 0.5
-end
-
-red.off
-green.off
-blue.off
-yellow.off
