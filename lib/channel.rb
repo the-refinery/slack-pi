2ae17ab77778bfd548892083497f2c1251ef5d7c
Dave Goerlich <dgoerlich@designinginteractive.com>
2014-03-01 03:34:45 +0000

    Initial proof of concept works. Polls the channel's latest 100 messages and notifies if something matches the keywords

--- lib/channel.rb
+++ lib/channel.rb
@@ -9,6 +9,49 @@ class Channel
     @led = led
   end
 
+  def poll
+    messages = get_messages
+
+    keyword_found = false
+
+    messages.each do |message|
+      unless message["text"].nil?
+        text = message["text"].downcase
+
+        KEYWORDS.each do |keyword|
+          if text.include? keyword.downcase
+            keyword_found = true
+          end
+        end
+      end
+    end
+
+    if keyword_found
+      notify
+    end
+
+  end
+
+private
+  
+  def get_messages
+    query = Array.new
+    query << "token=#{SLACK_TOKEN}"
+    query << "channel=#{@id}"
+
+    uri = URI.parse("https://slack.com/api/channels.history")
+    uri.query = query.join("&")
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+
+    data = JSON.parse(response.body)
+
+    data["messages"]
+  end
+
   def notify
     @led.blink
   end
--- slack-pi.rb
+++ slack-pi.rb
@@ -1,8 +1,21 @@
 require_relative 'lib/led'
+require_relative 'lib/channel'
 require_relative 'config/slack_config'
 
-red = Led.new "Red", 18
-green = Led.new "Green", 23
-blue = Led.new "Blue", 24
-yellow = Led.new "Yellow", 25
+@board = {
+  red:  Led.new("Red", 18),
+  green:  Led.new("Green", 23),
+  blue:  Led.new("Blue", 24),
+  yellow:  Led.new("Yellow", 25)
+}
+
+@channels = {}
+
+@monitored_channels.each_pair do |color, channel_id|
+  @channels[channel_id] = Channel.new(channel_id, @board[color])
+end
+
+@channels.each_value do |channel|
+  channel.poll
+end
 
