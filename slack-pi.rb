b354c48e302561ab2afd23ce4a14270c058d2a02
Dave Goerlich <dgoerlich@designinginteractive.com>
2014-02-28 19:37:04 -0500

    A quick wave test

--- slack-pi.rb
+++ slack-pi.rb
@@ -1,1 +1,25 @@
 require_relative 'lib/led'
+
+def flip led
+  if led.on?
+    led.off
+  else
+    led.on
+  end
+
+  sleep 0.1
+end
+
+red = Led.new "Red", 18
+green = Led.new "Green", 23
+blue = Led.new "Blue", 24
+yellow = Led.new "Yellow", 25
+
+0.step(11) do
+  flip red
+  flip green
+  flip blue
+  flip yellow
+
+  sleep 0.5
+end
